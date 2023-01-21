1a002beaa0d89e5a51a81f3ecb6659d19b78e157
MrJanHorak <janny_b_goode@hotmail.com>
2023-01-21 12:39:31 -0500

    set up JS variables

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let colors = ['red', 'green', 'purple', 'white','black', 'orange','blue']
+let pattern = ["","","",""] // let computer generate random pattern
+let guess = ["","","",""] 
+
+let hints = ["","","",""] // randomize the hints
+
+let score = 0
+let guesses = 0
+
+
+
