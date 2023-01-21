62ed26183be2ad2253bd5eb23101f8a13e28bc3c
MrJanHorak <janny_b_goode@hotmail.com>
2023-01-21 14:10:18 -0500

    create random guess pattern

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
-let colors = ['red', 'green', 'purple', 'white','black', 'orange','blue']
-let pattern = ["","","",""] // let computer generate random pattern
-let guess = ["","","",""] 
+let colors = ['red', 'green', 'purple', 'white', 'black', 'orange', 'blue']
+let pattern = ['', '', '', ''] // let computer generate random pattern
+let guess = ['', '', '', '']
 
-let hints = ["","","",""] // randomize the hints
+let hints = ['', '', '', ''] // randomize the hints
 
 let score = 0
 let guesses = 0
 
+console.log(colors)
+// set up game
+
+const shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
+
+const patternPicker = (arr) => {
+  for (let i = 0; i < arr.length ; i++){
+    arr[i] = colors[Math.floor(Math.random() * (colors.length))]
+  }
+}
 
 
+shuffle(colors)
+patternPicker(pattern)
+console.log(colors)
+console.log(pattern)
