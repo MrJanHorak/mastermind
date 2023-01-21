00ccd8a31272ccf55d5fa97b6656e537f7c23e99
MrJanHorak <janny_b_goode@hotmail.com>
2023-01-21 14:39:24 -0500

    create pattern compare function

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let colors = ['red', 'green', 'purple', 'white', 'black', 'orange', 'blue']
 let pattern = ['', '', '', ''] // let computer generate random pattern
-let guess = ['', '', '', '']
+let guess = ['red', 'green', 'green', 'blue']
 
 let hints = ['', '', '', ''] // randomize the hints
 
@@ -12,19 +12,33 @@ console.log(colors)
 
 const shuffle = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[arr[i], arr[j]] = [arr[j], arr[i]]
   }
 }
 
 const patternPicker = (arr) => {
-  for (let i = 0; i < arr.length ; i++){
-    arr[i] = colors[Math.floor(Math.random() * (colors.length))]
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = colors[Math.floor(Math.random() * colors.length)]
   }
 }
 
-
+const compGuessPat = (guess) => {
+  guess.forEach((ele, index) => {
+    if (guess[index] === pattern[index]) {
+      hints[index] = 'red'
+    }
+    if (pattern.includes(guess[index])) {
+      if (guess[index] !== pattern[index]) {
+        hints[index] = 'white'
+      }
+    }
+  })
+}
 shuffle(colors)
 patternPicker(pattern)
 console.log(colors)
-console.log(pattern)
+console.log("Pattern: ",pattern)
+console.log("Guess: ",guess)
+compGuessPat(guess)
+console.log("Hints: ",hints)
