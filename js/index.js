b80d049729d0acf2d34fff1c2c9e82acd92e9ff0
MrJanHorak <janny_b_goode@hotmail.com>
2023-01-21 22:27:43 -0500

    add hint divs

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@ let guesses = 0
 let maxGuesses = 10
 
 // create game field on DOM
-const main = document.querySelector("body")
-const gameContainerDiv = document.createElement("div")
-const gameHeader = document.createElement("h1")
-const patternDiv = document.createElement("div")
-const gameBoardDiv = document.createElement("div")
-const hintsDiv = document.createElement("div")
-const guessDiv = document.createElement("div")
-const scoreDiv = document.createElement("div")
+const main = document.querySelector('body')
+const gameContainerDiv = document.createElement('div')
+const gameHeader = document.createElement('h1')
+const patternDiv = document.createElement('div')
+const gameBoardDiv = document.createElement('div')
+
+const guessDiv = document.createElement('div')
+const scoreDiv = document.createElement('div')
 
 // set up game
 
@@ -46,41 +46,54 @@ const compGuessPat = (guess) => {
   })
 }
 
-
 shuffle(colors)
 patternPicker(pattern)
 console.log(colors)
-console.log("Pattern: ",pattern)
-console.log("Guess: ",guess)
+console.log('Pattern: ', pattern)
+console.log('Guess: ', guess)
 compGuessPat(guess)
-console.log("Hints: ",hints)
+console.log('Hints: ', hints)
 
 // fill the div
-patternDiv.className ="patternDiv"
-gameHeader.textContent = "Master Mind"
+patternDiv.className = 'patternDiv'
+gameHeader.textContent = 'Master Mind'
 gameContainerDiv.appendChild(gameHeader)
 main.appendChild(gameContainerDiv)
 
 // create div to hold pattern to guess
 pattern.forEach((ele, index) => {
-  let colorPeg = document.createElement("div")
-  colorPeg.className ="pattern"
+  let colorPeg = document.createElement('div')
+  colorPeg.className = 'pattern'
   colorPeg.style.backgroundColor = ele
   patternDiv.appendChild(colorPeg)
   gameContainerDiv.appendChild(patternDiv)
 })
 
 // create div to hold all the guesses
-for (let i = 0; i < maxGuesses ; i++){
-  const guessInput = document.createElement("div")
-  guessInput.className = "patternDiv"
-  for (let j = 0; j < pattern.length; j++){
-    const guessPeg = document.createElement("div")
-    guessPeg.className = "pattern"
+for (let i = 0; i < maxGuesses; i++) {
+  const guessInput = document.createElement('div')
+  guessInput.className = 'patternGuessDiv'
+  
+  const hintsDiv = document.createElement('div')
+  hintsDiv.className = 'hintDiv'
+  hintsDiv.id = `hintsRow${i}`
+
+  for (let h = 0; h < pattern.length; h++) {
+    const hintPeg = document.createElement('div')
+    hintPeg.className = "hint"
+    hintPeg.id = `hr${i}hi${h}`
+    hintsDiv.appendChild(hintPeg)
+  }
+  guessInput.appendChild(hintsDiv)
+
+  for (let j = 0; j < pattern.length; j++) {
+    const guessPeg = document.createElement('div')
+    guessPeg.className = 'pattern'
     guessPeg.id = `gr${i}gc${j}`
-    guessPeg.style.backgroundColor = "black"
+    guessPeg.style.backgroundColor = 'black'
     guessInput.appendChild(guessPeg)
   }
-  gameContainerDiv.appendChild(guessInput)
+  guessDiv.appendChild(guessInput)
+  guessDiv.className = 'guessDiv'
 }
-
+gameContainerDiv.appendChild(guessDiv)
